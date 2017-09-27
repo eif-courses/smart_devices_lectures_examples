49be57529e0d9b3a3c7bf5798e63e65dec7ff78a
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2017-09-27 11:01:57 +0300

    Appended lecture list to 20

---
+++ Lecture 6/src/main/java/lambda/Lambdas.java
@@ -0,0 +1,18 @@
+package lambda;
+import java.util.*;
+public class Lambdas {
+
+    public static void main(String[] args) {
+        String jsonObject = "{\"value\": 50, \"name\": \"Petras\"}";
+        String ll = String.format("<html>\n<table>Hello</table>\n<br/>\n</html>");
+
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("goodbye");
+        System.out.println("Sveiki studentai : )");
+
+        //List<String> list = new ArrayList<>();
+
+
+    }
+}
