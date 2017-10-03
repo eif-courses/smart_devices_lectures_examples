f06615c3aeb500b37d4c37f91015d362e2bfe651
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2017-10-03 09:22:38 +0300

    Appended lecture list to 20

--- Lecture 7/src/main/java/Main.java
+++ Lecture 7/src/main/java/Main.java
@@ -1,4 +1,8 @@
+import com.sun.xml.internal.stream.util.ThreadLocalBufferAllocator;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 interface Spalva{
     void nuspalvoti(Gyvunas gyvunas);
@@ -23,8 +27,52 @@ class SpalvinameGyvuna implements Spalva{
     }
 }
 
+class Procesas extends Thread{
+    @Override
+    public void run() {
+        System.out.println("Pagrindinis procesas");
+    }
+}
+
+class KitasProcesas extends Gyvunas implements Runnable, Spalva{
+    public KitasProcesas(String pav) {
+        super(pav);
+    }
+    @Override
+    public void run() {
+        System.out.println("Kitas procesas.");
+    }
+    @Override
+    public void nuspalvoti(Gyvunas gyvunas) {
+
+    }
+}
+
+
+
 public class Main {
     public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Anonimine procesas paleistas");
+            }
+        };
+        Runnable kitas = () -> System.out.println("Lambda procesas paleistas");
+
+        new Thread(runnable).start();
+        new Thread(kitas).start();
+        new Procesas().start();
+        new Thread(new KitasProcesas("Kitas")).start();
+
+        JFrame jFrame = new JFrame();
+        JButton j = new JButton("OK");
+        j.addActionListener(e -> JOptionPane.showMessageDialog(null, "Mygtukas paspaustas"));
+        jFrame.add(j);
+        jFrame.pack();
+        jFrame.setVisible(true);
+
         Gyvunas lape = new Gyvunas("Lape");
         Gyvunas vilkas = new Gyvunas("Vilkas");
         SpalvinameGyvuna spalvinam = new SpalvinameGyvuna();
@@ -38,11 +86,25 @@ public class Main {
             return pav.substring(5);
         };
         System.out.println("rez: "+kit.pavadinimas("Laba diena"));
-
+        LambdaInterfeisas la = (a, b) -> a + (int)2.5;
+//final int k = 10;
 
     }
 
     interface Kitas{
          String pavadinimas(String k);
     }
+    @FunctionalInterface
+    interface LambdaInterfeisas{
+        int skaiciuoti(int a, int b);
+        default void sk(){
+            System.out.println("Helllo");
+        }
+        static void sudeti(int a, int b){
+            System.out.println(a+b);
+        }
+       // void kitas();
+    }
+
+
 }
