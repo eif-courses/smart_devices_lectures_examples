d10b67a1cf51beb251c87d8d080fc459f561ac38
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2017-10-04 21:37:13 +0300

    Appended lecture list to 20

--- Lecture 7/src/main/java/Main.java
+++ Lecture 7/src/main/java/Main.java
@@ -3,6 +3,12 @@ import com.sun.xml.internal.stream.util.ThreadLocalBufferAllocator;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.*;
 
 interface Spalva{
     void nuspalvoti(Gyvunas gyvunas);
@@ -14,6 +20,9 @@ class Gyvunas{
     }
     public String getPav() {
         return pav;
+    }
+    void randomGyvunas(){
+        System.out.println("RANDOM GYVUNAS");
     }
     void spalvok(Gyvunas gyvunas, Spalva spalva){
         spalva.nuspalvoti(gyvunas);
@@ -47,7 +56,20 @@ class KitasProcesas extends Gyvunas implements Runnable, Spalva{
     }
 }
 public class Main {
+
+    public static void spausdintiGyvunusPagalPirmaRaide(List<Gyvunas> list,
+                                                        Predicate<Gyvunas> predicate, java.util.function.Consumer<Gyvunas> consumer){
+        for (Gyvunas gyvunas : list) {
+            if(predicate.test(gyvunas)){
+                consumer.accept(gyvunas);
+                // sout
+            }
+        }
+    }
+
     public static void main(String[] args) {
+
+
 
         Runnable runnable = new Runnable() {
             @Override
@@ -69,13 +91,38 @@ public class Main {
         jFrame.pack();
         jFrame.setVisible(true);
 
+        List<Gyvunas> gyvunai = new ArrayList<>();
         Gyvunas lape = new Gyvunas("Lape");
         Gyvunas vilkas = new Gyvunas("Vilkas");
+        gyvunai.add(lape);
+        gyvunai.add(vilkas);
+        gyvunai.add(lape);
+        gyvunai.add(vilkas);
+        gyvunai.add(lape);
+        gyvunai.add(vilkas);
+
+       Consumer<Gyvunas> gyvunasConsumer = System.out::println;
+       Predicate<Gyvunas> predicate = gyvunas -> gyvunas.getPav().startsWith("l");
+       Consumer<Gyvunas> lapesCons = Gyvunas::randomGyvunas;
+       lapesCons.accept(lape);
+       String s [] = {"wea", "wae"};
+        Arrays.sort(s, String::compareTo);
+       // Comparator
+       // int isorinis = 50;
+       BiConsumer<Integer, Integer> biConsumer = (a, b) -> System.out.println(a / b );
+
+
+       biConsumer.accept(1,0);
+
+       //gyvunasConsumer.accept(lape);
+
+        spausdintiGyvunusPagalPirmaRaide(gyvunai, predicate, gyvunasConsumer);
+
+
+
         SpalvinameGyvuna spalvinam = new SpalvinameGyvuna();
         lape.spalvok(lape, spalvinam);
         vilkas.spalvok(vilkas, spalvinam);
-
-
         vilkas.spalvok(vilkas, (gyvunas)-> System.out.println(gyvunas.getPav()));
         Kitas kit = (pav)-> {
            // System.out.println(pav.charAt(5));
@@ -83,6 +130,8 @@ public class Main {
         };
         System.out.println("rez: "+kit.pavadinimas("Laba diena"));
         LambdaInterfeisas la = (a, b) -> a + (int)2.5;
+        Runnable r = () -> System.out.println("aweaew");
+        new Thread(r).start();
     }
     interface Kitas{
          String pavadinimas(String k);
@@ -98,6 +147,11 @@ public class Main {
         }
        // void kitas();
     }
+    interface GyvunasI{
+        Gyvunas gyv();
+    }
+
+
 
 
 }
